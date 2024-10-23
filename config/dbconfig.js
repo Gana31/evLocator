62649adaef65f5f4cbea11b1701c5a9c9b333971
Gana31 <139262802+Gana31@users.noreply.github.com>
2024-10-23 16:31:54 +0530

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 export default async function connectToDatabase() {
   try {
-    const data = await mongoose.connect(process.env.DATABASE_URL);
+    const data = await mongoose.connect(`${process.env.DATABASE_URL}/project`);
     console.log(`Database server connected at port: ${data.connection.port}`);
     console.log(`Database server connected at host: ${data.connection.host}`);
   } catch (error) {
